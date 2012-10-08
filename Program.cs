d71608366ef556b052c8777fbcf019a22a73fb6f
Tony <js.pwns@gmail.com>
2012-10-07 21:07:37 -0400

    [3] Cleanup, refactoring, assimilating, constructing additional pylons, the usual.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 
-namespace BF2GamespyLoginEmulator
+namespace Gamespy
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Run the server
-            Server s = new Server();
+            Server iServer;
+
+            try
+            {
+                iServer = new Server();
+                iServer.Start();
+
+            }
+            catch( System.Net.Sockets.SocketException e )
+            {
+                Console.WriteLine( e.ToString() );
+            }
+            catch( System.Threading.ThreadInterruptedException e )
+            {
+                Console.WriteLine( e.ToString() );
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( e.ToString() );
+            }
         }
     } 
 }
--- Server.cs
+++ Server.cs
@@ -1,18 +1,103 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
-namespace BF2GamespyLoginEmulator
+namespace Gamespy
 {
-    class Server
+    public sealed class Server
     {
+        private Socket BF2ASocket;
+        private TcpListener GPCMListener, GPSPListener;
+        private Thread BF2AThread, GPCMThread, GPSPThread, InputThread;
+        bool Shutdown = false;
+
         public Server()
         {
             // Init the socket classes here
+            BF2ASocket = new Socket( AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp );
+            GPCMListener = new TcpListener( IPAddress.Loopback, 29900 );
+            GPSPListener = new TcpListener( IPAddress.Loopback, 29901 );
+        }
 
-            // Main program loop
-            while (true) 
-            { 
-                // Server is running.
-                // Should allow for input commands
+        public void Start()
+        {
+            BF2ASocket.Bind( new IPEndPoint( IPAddress.Loopback, 27900 ) );
+            GPSPListener.Start();
+            GPCMListener.Start();
+
+            Console.Write
+            (
+                "<BF2A> successfully bound to {0}" + Environment.NewLine +
+                "<GPCM> successfully bound to {1}" + Environment.NewLine +
+                "<GPSP> successfully bound to {2}" + Environment.NewLine,
+                BF2ASocket.LocalEndPoint, GPCMListener.LocalEndpoint, GPSPListener.LocalEndpoint
+            );
+
+            InputThread = new Thread( InputLoop );
+            BF2AThread = new Thread( BF2ALoop );
+            GPCMThread = new Thread( GPCMLoop );
+            GPSPThread = new Thread( GPSPLoop );
+
+            BF2AThread.IsBackground = true;
+            GPCMThread.IsBackground = true;
+            GPSPThread.IsBackground = true;
+            InputThread.IsBackground = false;
+
+            BF2AThread.Name = "BF2A Listner";
+            GPCMThread.Name = "GPCM Listner";
+            GPSPThread.Name = "GPSP Listner";
+            InputThread.Name = "Input Listener";
+
+            BF2AThread.Start();
+            GPCMThread.Start();
+            GPSPThread.Start();
+            InputThread.Start();
+        }
+
+        public void Stop()
+        {
+            Shutdown = true;
+            Console.WriteLine( "Stopped." );
+        }
+
+        private void BF2ALoop()
+        {
+            //...
+        }
+
+        private void GPCMLoop()
+        {
+            //...
+        }
+
+        private void GPSPLoop()
+        {
+            //...
+        }
+
+        private void InputLoop()
+        {
+            while( !Shutdown )
+            {
+                if( !Console.KeyAvailable )
+                    continue;
+
+                string Line = Console.ReadLine();
+
+                if( string.IsNullOrWhiteSpace( Line ) )
+                    continue;
+
+                //Basic input logic.
+                if( Line.ToLower().Trim() == "q" || Line.ToLower().Trim() == "quit" || Line.ToLower().Trim() == "x" || Line.ToLower().Trim() == "exit" )
+                    Stop();
+                else
+                {
+                    lock( Console.Out )
+                    {
+                        Console.WriteLine( "Unrecognized input '{0}'", Line );
+                    }
+                }
             }
         }
     }
