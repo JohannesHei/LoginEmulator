5bdce744f8cd6a87ad499c0511b5211c502aea33
Steven Wilson <wilson.steven10@yahoo.com>
2012-10-08 16:07:35 -0500

    [10] added Database class, and worked on more gpCM

--- BF2GamespyLoginEmulator/ClientCM.cs
+++ BF2GamespyLoginEmulator/ClientCM.cs
@@ -25,19 +25,15 @@ namespace Gamespy
              // Init a new client stream class
             Stream = new ClientStream(client);
 
-            switch (Step)
-            {
-                case 0:
-                    SendServerChallenge();
-                    Step++;
-                    break;
-                case 1:
-                    break;
-            }
+            // Start off by sending the client a challenge
+            SendServerChallenge();
         }
+
+        #region Steps
 
         public void SendServerChallenge()
         {
+            // First we need to create a random string the length of 10 characters
             char[] buffer = new char[10];
             rand = new Random((int)DateTime.Now.Ticks);
 
@@ -46,11 +42,76 @@ namespace Gamespy
                 buffer[i] = chars[rand.Next(chars.Length)];
             }
 
+            // Next we send the client the challenge key
             string key = buffer.ToString();
-
             Stream.Write( String.Format("\\lc\\1\\challenge\\{0}\\id\\1\\final\\", key) );
 
-            Console.WriteLine(Stream.Read());
+            // Process the login information returned by the client
+            RecvLogin();
         }
+
+        public void SendProof(string[] info)
+        {
+
+        }
+
+        private void RecvLogin()
+        {
+            string message = Stream.Read();
+            System.Diagnostics.Debug.Write("Challenge response: {0}", message);
+
+            // Create an array by spliting the returned data
+            string[] recv = message.Split('\\');
+            string[] info;
+
+            switch (recv.Length)
+            {
+                case 18:
+                    if (recv[0] == "newuser")
+                    {
+                        HandleNewUser(recv);
+                        break;
+                    }
+
+                    info = new string[3] { 
+                        recv[4], // Nick
+                        recv[2], // Challenge
+                        recv[6]  // Response
+                    };
+                    SendProof(info);
+                    break;
+
+                case 24: // BF 1.5
+                    // 1.5 == true
+                case 22:
+                    info = new string[3] { 
+                        recv[4], // Nick
+                        recv[2], // Challenge
+                        recv[10]  // Response
+                    };
+                    SendProof(info);
+                    break;
+
+                case 20: // BF 1.5
+                    info = new string[3] { 
+                        recv[4], // Nick
+                        recv[2], // Challenge
+                        recv[6]  // Response
+                    };
+                    SendProof(info);
+                    break;
+            }
+        }
+
+        #endregion Steps
+
+        #region User Methods
+
+        private void HandleNewUser(string[] recv)
+        {
+
+        }
+
+        #endregion
     }
 }
---
+++ BF2GamespyLoginEmulator/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace Gamespy
+{
+    class Database
+    {
+        public Database()
+        {
+
+        }
+
+        public Dictionary <string, string>GetUserData(string nick)
+        {
+
+        }
+    }
+}
