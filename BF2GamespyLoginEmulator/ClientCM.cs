a341ff3f356bfa28e3c1da89c564fb7d430ac2ec
Steven Wilson <wilson.steven10@yahoo.com>
2012-10-08 18:03:01 -0500

    [11] Updated the ClientCM

--- BF2GamespyLoginEmulator/ClientCM.cs
+++ BF2GamespyLoginEmulator/ClientCM.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
 namespace Gamespy
 {
+    /*
+     *  Gamespy Client Manager
+     *  gpcm.gamespy.com
+     */
     class ClientCM
     {
         private ClientStream Stream;
@@ -14,6 +18,27 @@ namespace Gamespy
         private int Step = 0;
         private Random rand;
         private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static string[] BtoH = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0a", "0b", "0c", "0d", "0e", "0f",
+                                  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "1a", "1b", "1c", "1d", "1e", "1f",
+                                  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "2a", "2b", "2c", "2d", "2e", "2f",
+                                  "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "3a", "3b", "3c", "3d", "3e", "3f",
+                                  "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "4a", "4b", "4c", "4d", "4e", "4f",
+                                  "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5a", "5b", "5c", "5d", "5e", "5f",
+                                  "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "6a", "6b", "6c", "6d", "6e", "6f",
+                                  "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "7a", "7b", "7c", "7d", "7e", "7f",
+                                  "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "8a", "8b", "8c", "8d", "8e", "8f",
+                                  "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "9a", "9b", "9c", "9d", "9e", "9f",
+                                  "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "aa", "ab", "ac", "ad", "ae", "af",
+                                  "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "ba", "bb", "bc", "bd", "be", "bf",
+                                  "c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "ca", "cb", "cc", "cd", "ce", "cf",
+                                  "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "da", "db", "dc", "dd", "de", "df",
+                                  "e0", "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "ea", "eb", "ec", "ed", "ee", "ef",
+                                  "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "fa", "fb", "fc", "fd", "fe", "ff" };
+        static char[] alphanumeric = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 
         public ClientCM(TcpClient client)
@@ -52,54 +77,32 @@ namespace Gamespy
 
         public void SendProof(string[] info)
         {
-
+            Console.WriteLine("Username: {0}; Challenge: {1};", info[0], info[1]);
+            Stream.Write("\\error\\\\err\\260\\fatal\\\\errmsg\\The password provided is incorrect.\\id\\1\\final\\");
         }
 
         private void RecvLogin()
         {
-            string message = Stream.Read();
-            System.Diagnostics.Debug.Write("Challenge response: {0}", message);
+            string message = Stream.Read().TrimEnd();
+            Console.WriteLine("Challenge response: {0}", message);
 
             // Create an array by spliting the returned data
-            string[] recv = message.Split('\\');
+            char[] backslash = { '\\' };
+            string[] recv = message.Split(backslash);
             string[] info;
 
-            switch (recv.Length)
+            if (recv[0] == "newuser")
             {
-                case 18:
-                    if (recv[0] == "newuser")
-                    {
-                        HandleNewUser(recv);
-                        break;
-                    }
-
-                    info = new string[3] { 
-                        recv[4], // Nick
-                        recv[2], // Challenge
-                        recv[6]  // Response
-                    };
-                    SendProof(info);
-                    break;
-
-                case 24: // BF 1.5
-                    // 1.5 == true
-                case 22:
-                    info = new string[3] { 
-                        recv[4], // Nick
-                        recv[2], // Challenge
-                        recv[10]  // Response
-                    };
-                    SendProof(info);
-                    break;
-
-                case 20: // BF 1.5
-                    info = new string[3] { 
-                        recv[4], // Nick
-                        recv[2], // Challenge
-                        recv[6]  // Response
-                    };
-                    SendProof(info);
-                    break;
+                HandleNewUser(recv);
+            }
+            else
+            {
+                info = new string[3] { 
+                    getParameterValue(recv, "uniquenick"), // Nick
+                    getParameterValue(recv, "challenge"),  // Challenge
+                    getParameterValue(recv, "response")    // Response
+                };
+                SendProof(info);
             }
         }
 
@@ -110,6 +113,50 @@ namespace Gamespy
         private void HandleNewUser(string[] recv)
         {
 
+        }
+
+        #endregion
+
+
+
+        #region Misc Methods
+
+        private string getParameterValue(string[] parts, string parameter)
+        {
+            bool next = false;
+            foreach (string part in parts)
+            {
+                if (next) 
+                    return part;
+                else if (part == parameter) 
+                    next = true;
+            }
+            return "";
+        } 
+
+        private string getResponseValue(string nickname, string password, string challenge1, string challenge2)
+        {
+            // Generate a MD5 password hash, and convert it to Gamespy's specific hasing method
+            MD5 createMD5 = MD5.Create();
+            byte[] passwordHash = createMD5.ComputeHash(Encoding.ASCII.GetBytes(password));
+            string pwmd5 = "";
+            foreach (byte b in passwordHash)
+                pwmd5 += BtoH[b];
+
+            // Generate our string to be hashed
+            string hash = pwmd5;
+            for (int i = 0; i < 48; i++)
+                hash += " ";
+            hash += nickname + challenge1 + challenge2 + pwmd5;
+
+            // Create our final hash, and then convert that hash to a string
+            byte[] finalHash = createMD5.ComputeHash(Encoding.ASCII.GetBytes(hash));
+            StringBuilder result = new StringBuilder();
+            foreach (byte b in finalHash)
+            {
+                result.AppendFormat("{0}", BtoH[b]);
+            }
+            return result.ToString();
         }
 
         #endregion
