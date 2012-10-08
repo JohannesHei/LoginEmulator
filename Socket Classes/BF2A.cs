4a26904ce0c3aeddc3d6efce6f5e317d86b8b470
Steven Wilson <wilson.steven10@yahoo.com>
2012-10-07 19:18:02 -0500

    [2] Updated the BF2a class

--- Socket Classes/BF2A.cs
+++ Socket Classes/BF2A.cs
@@ -9,9 +9,19 @@ namespace BF2GamespyLoginEmulator
         // Our binding port
         public const int Port = 27900;
 
+        // Socket
+        Socket listener;
+
         public BF2A()
         {
-            // Suppossed to be a UDP port!
+            // Create our end point for binding
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            // Init the socket
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the listener
+            listener.Bind(localEndPoint);
 
             // Write to the console the success message
             Console.WriteLine("<BF2Available> bound to port: " + Port.ToString());
