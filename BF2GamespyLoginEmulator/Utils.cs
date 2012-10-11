df164ce9360e9651dfab5c2aafa1518bbe4e5953
Syke94 <js.pwns@gmail.com>
2012-10-10 22:31:26 -0400

    [20] Fiddling with password encoding.
    * Base64.cs has been removed temporarily, passwords are run thru an external tool to achieve the same.
    * Two new methods in Utils.cs, EncodeGamespyPassword( string ) and DecodeGamespyPassword( string ).

--- BF2GamespyLoginEmulator/Utils.cs
+++ BF2GamespyLoginEmulator/Utils.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.IO;
+using System.Diagnostics;
 
 namespace Gamespy
 {
@@ -22,6 +23,36 @@ namespace Gamespy
             }
 
             return Result;
+        }
+
+        public static string EncodeGamespyPassword( string Password )
+        {
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.UseShellExecute = false;
+            Info.CreateNoWindow = true;
+            Info.RedirectStandardOutput = true;
+            Info.Arguments = string.Format( "e \"{0}\"", Password );
+            Info.FileName = Path.Combine( Utils.AssemblyPath, "gspassenc.exe" );
+
+            Process gsProcess = Process.Start( Info );
+            string EncodedPassword = gsProcess.StandardOutput.ReadToEnd();
+
+            return EncodedPassword;
+        }
+
+        public static string DecodeGamespyPassword( string Password )
+        {
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.UseShellExecute = false;
+            Info.CreateNoWindow = true;
+            Info.RedirectStandardOutput = true;
+            Info.Arguments = string.Format( "d \"{0}\"", Password );
+            Info.FileName = Path.Combine( Utils.AssemblyPath, "gspassenc.exe" );
+
+            Process gsProcess = Process.Start( Info );
+            string DecodedPassword = gsProcess.StandardOutput.ReadToEnd();
+
+            return DecodedPassword;
         }
     }
 }
